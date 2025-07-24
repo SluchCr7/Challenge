da957099963ca73796212dd43ae0ebe9c34506b1
Sluch <ahmedking10710@gmail.com>
2025-07-24 21:30:21 +0300

    some basic edits in front end

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose')
 
 const ConnectDb = async () => {
-    mongoose.connect(process.env.MONGO_URI)
-        .then(() => {
-            console.log('Database Connected')
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1);
+    }
 }
 
 module.exports = ConnectDb
